9472aabb2df293f8b86afa9193cb235f9b6fc546
Alexander Serrano <cyasam7@gmail.com>
2022-02-27 16:15:59 -0600

    cambios en el definition

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
     useEffect(() => {
         dispatch(Actions.getInitialCharacters());
         //dispatch(Actions.listCharacters());
+        console.log("Hola");
     }, [dispatch]);
 
     return (
